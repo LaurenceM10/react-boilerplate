5d7b8a756d05041e1a7100bfd53f64b86fa3517d
Dalex19 <dearalex875@gmail.com>
2022-05-21 17:46:02 -0600

    refactor: routes

--- src/app-view.tsx
+++ src/app-view.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./features/login/views";
+import RegisterPage from "./features/register/index";
 
 function AppView() {
     return (
         <Router>
             <div className="app">
-                <Switch>
-                    <Route exact path="/login">
-                        <LoginPage />
-                    </Route>
-                </Switch>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />   
+                </Routes>
             </div>
         </Router>
     );
